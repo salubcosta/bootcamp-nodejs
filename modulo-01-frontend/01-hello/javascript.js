6d24fb5fe571b41bc6bf43fedefcf05335f41f70
Salumao Barbosa da Costa <salubcosta@gmail.com>
2023-03-13 16:12:12 -0300

    Modulo 01 - Fundamentos - Front end

---
+++ modulo-01-frontend/01-hello/javascript.js
@@ -0,0 +1,13 @@
+var aluno = {
+  matricula: 72531,
+  nome: "Salumão Barbosa",
+  curso: "Bootcamp NODEJS - FRONT END part01",
+  ativo: true,
+};
+
+(function () {
+  console.log(aluno.nome);
+})();
+
+var frutas = ["Uva", "Laranja", "Maçã"];
+frutas.push("Melancia");
